f224868186e99e67e1b03e6abcb6b4386a48437b
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 17:59:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
   hash[key]
+  puts hash
+  puts key
+  puts hash[key]
 end
 
 def update_counting_hash(hash, key)
