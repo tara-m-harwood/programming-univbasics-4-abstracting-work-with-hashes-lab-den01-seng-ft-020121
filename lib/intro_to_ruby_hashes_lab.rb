19f6d2b484d4b88e84135f14399fe86bec857854
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 18:03:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,10 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {:key => value}
+  {key => value}
 end
 
 def read_from_hash(hash, key)
   hash[key]
-  puts hash
-  puts key
-  puts hash[key]
 end
 
 def update_counting_hash(hash, key)
